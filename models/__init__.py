e21c2a4685c7e1ae4b8bec52271067770f9609e4
oscarsidsa81 <ovilches@sid-sa.com>
2025-07-07 16:29:25 +0200

    nuevo archivo para reflejar campos del form de sale.order

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,1 @@
-from . import sid_sale_form
 from . import sid_sale_order
--- models/sid_sale_order.py
+++ models/sid_sale_order.py
@@ -3,6 +3,12 @@ from odoo import fields, models, api
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
+    x_aval = fields.Boolean(
+        string="Avales",
+        tracking=1,
+        store=True
+    )
+
     x_restante = fields.Monetary(
         string="Restante",
         compute="_compute_x_restante",
@@ -12,20 +18,6 @@ class SaleOrder(models.Model):
         currency_field='currency_id'
     )
 
-    @api.depends('order_line.qty_delivered', 'order_line.price_reduce', 'order_line.product_uom_qty',
-                 'order_line.product_id')
-    def _compute_x_restante(self):
-        for record in self:
-            total_base = 0.0
-            for line in record.order_line:
-                if line.product_id.default_code != "Down payment":
-                    if line.qty_delivered < line.product_uom_qty:
-                        total_base += (line.product_uom_qty - line.qty_delivered) * line.price_reduce
-            record.x_restante = total_base
-
-    class SaleOrder ( models.Model ) :
-        _inherit = 'sale.order'
-
     x_excesos = fields.Monetary(
         string="Excesos Pend.",
         compute="_compute_x_excesos",
@@ -34,6 +26,44 @@ class SaleOrder(models.Model):
         currency_field='currency_id',
         help="Para cálculo de excesos de metraje",
     )
+
+    x_hitos_pendientes = fields.Monetary(
+        string="Hitos pendientes",
+        compute="_compute_x_hitos_pendientes",
+        store=True,
+        currency_field='currency_id',
+        help="Para cálculo de Hitos Pendientes del campo Down Payment"
+    )
+
+    x_pendiente = fields.Monetary(
+        string="Base Pendiente",
+        compute="_compute_x_pendiente",
+        help="Base imponible que queda por facturar y ya está entregado",
+        store=True,
+        readonly=True,
+        currency_field='currency_id'
+    )
+
+    x_total = fields.Monetary(
+        string="Base Total Facturada",
+        compute="_compute_x_total",
+        help="Base facturada por línea de venta",
+        store=True,
+        readonly=True,
+        currency_field='currency_id'
+    )
+
+    # === MÉTODOS ===
+
+    @api.depends('order_line.qty_delivered', 'order_line.price_reduce', 'order_line.product_uom_qty', 'order_line.product_id')
+    def _compute_x_restante(self):
+        for record in self:
+            total_base = 0.0
+            for line in record.order_line:
+                if line.product_id.default_code != "Down payment":
+                    if line.qty_delivered < line.product_uom_qty:
+                        total_base += (line.product_uom_qty - line.qty_delivered) * line.price_reduce
+            record.x_restante = total_base
 
     @api.depends('order_line.qty_delivered', 'order_line.qty_invoiced', 'order_line.price_reduce',
                  'order_line.product_uom_qty', 'order_line.product_id')
@@ -48,78 +78,34 @@ class SaleOrder(models.Model):
                         total_base += (line.qty_delivered - line.qty_invoiced) * line.price_reduce
             record.x_excesos = total_base
 
-            x_hitos_pendientes = fields.Monetary(
-                string="Hitos pendientes",
-                compute="_compute_x_hitos_pendientes",
-                store=True,
-                currency_field='currency_id',
-                help="Para cálculo de Hitos Pendientes del campo Down Payment"
-            )
-
-    class SaleOrder ( models.Model ) :
-        _inherit = 'sale.order'
-
+    @api.depends('order_line.product_id', 'order_line.product_uom_qty', 'order_line.qty_delivered', 'order_line.qty_invoiced')
     def _compute_x_hitos_pendientes(self):
         for record in self:
-            total_down_payment = 0.0  # Inicializamos la variable para sumar las líneas con producto 10987
+            total_down_payment = 0.0
             for line in record.order_line:
-                # Si el producto es 10987, la cantidad es mayor que 0 y no está facturado
                 if line.product_id.default_code == "Down payment" and line.product_uom_qty > 0 and line.qty_delivered == 0 and line.qty_invoiced == 0:
                     total_down_payment += (line.product_uom_qty - line.qty_delivered) * line.price_reduce
-            # Asignamos el valor calculado al campo 'x_hitos'
-            record.write({'x_hitos_pendientes': total_down_payment})
+            record.x_hitos_pendientes = total_down_payment
 
-    class SaleOrder ( models.Model ) :
-        _inherit = 'sale.order'
-
-    x_pendiente = fields.Monetary(
-        string="Base Pendiente",
-        compute="_compute_x_pendiente",
-        help="Base imponible que queda por facturar y ya está entregado",
-        store=True,
-        readonly=True,
-        currency_field='currency_id'
-    )
-
-    @api.depends('order_line.qty_to_invoice', 'order_line.price_reduce', 'invoice_ids', 'order_line.qty_delivered', 'order_line.product_uom_qty')
-
+    @api.depends('order_line.qty_to_invoice', 'order_line.price_reduce', 'invoice_ids', 'order_line.qty_delivered', 'order_line.product_uom_qty', 'order_line.qty_invoiced', 'order_line.product_id')
     def _compute_x_pendiente(self):
         for record in self:
             total_base_ex = 0.0
             for line in record.order_line:
-                # Verificamos si el producto tiene un ID diferente de 10987
                 if line.product_id.default_code != "Down payment":
-                    # Si la cantidad entregada es mayor que la cantidad pedida, restamos la diferencia
                     if line.qty_delivered > line.product_uom_qty and line.qty_invoiced <= line.product_uom_qty:
                         total_base_ex += (line.qty_delivered - line.product_uom_qty) * line.price_reduce
-                        # Si la cantidad facturada es mayor que la cantidad pedida, restamos la diferencia
                     elif line.qty_invoiced < line.qty_delivered and line.qty_invoiced > line.product_uom_qty:
                         total_base_ex += (line.qty_delivered - line.qty_invoiced) * line.price_reduce
-                        # En caso contrario, no se suma nada
-                    else:
-                        total_base_ex += 0.0
             total_base = 0.0
             for line in record.order_line:
                 total_base += line.qty_to_invoice * line.price_reduce
-            record.write({'x_pendiente': total_base - total_base_ex})
+            record.x_pendiente = total_base - total_base_ex
 
-    class SaleOrder ( models.Model ) :
-        _inherit = 'sale.order'
-
-    x_total = fields.Monetary(
-        string="Base Total Facturada",
-        compute="_compute_x_total",
-        help="Base facturada por línea de venta",
-        store=True,
-        readonly=True,
-        currency_field='currency_id'
-    )
-
-    @api.depends('order_line.qty_invoiced', 'order_line.price_unit', 'invoice_ids')
-
+    @api.depends('order_line.qty_invoiced', 'order_line.price_reduce_taxexcl')
     def _compute_x_total(self):
         for record in self:
             total_base = 0.0
             for line in record.order_line:
                 total_base += line.qty_invoiced * line.price_reduce_taxexcl
-            record.write({'x_total': total_base})
+            record.x_total = total_base
