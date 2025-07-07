f6c268420b9c99f4961d366289465015a59663c9
oscarsidsa81 <ovilches@sid-sa.com>
2025-07-07 16:25:33 +0200

    nuevo archivo para reflejar campos del form de sale.order

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,2 @@
-from . import sid_sale_order
 from . import sid_sale_form
+from . import sid_sale_order
--- models/sid_sale_form.py
+++ models/sid_sale_form.py
@@ -1,7 +1,6 @@
 from odoo import fields, models, api
 
 class SaleOrder(models.Model):
-
     _inherit = 'sale.order'
 
     x_aval = fields.Boolean(
