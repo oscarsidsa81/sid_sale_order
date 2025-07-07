24c2f9556630b87027d4b3e0445f7a04bbe573fc
oscarsidsa81 <ovilches@sid-sa.com>
2025-07-07 16:37:40 +0200

    incluyo resto de campos que quedan personalizados

--- models/sid_sale_order.py
+++ models/sid_sale_order.py
@@ -3,6 +3,44 @@ from odoo import fields, models, api
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
+    # 1. x_costes – tipo: Monetario
+    x_costes = fields.Monetary (
+        string="Costes adicionales",
+        currency_field='currency_id',
+        store=True,
+        copy=True
+    )
+
+    # 2. x_dossier – tipo: Booleano
+    x_dossier = fields.Boolean (
+        string="Tiene Dossier",
+        help="Campo para saber si se creó un dossier para este nº de contrato",
+        store=True
+    )
+
+    # 3. x_invoice_out – tipo: Booleano
+    x_invoice_out = fields.Boolean (
+        string="Facturado en otra factura",
+        help="Campo ayuda",
+        store=True
+    )
+
+    # 4. x_margen – tipo: Float (número flotante calculado)
+    x_margen = fields.Float (
+        string="Margen revisado",
+        compute="_compute_x_margen",
+        store=True,
+        readonly=True,
+        tracking=1
+    )
+
+    # 5. x_sale_order_id_sale_activity_count – tipo: Entero (calculado)
+    x_sale_order_id_sale_activity_count = fields.Integer (
+        string="Sale order count in Activities",
+        compute="_compute_sale_activity_count",
+        store=True
+    )
+
     x_aval = fields.Boolean(
         string="Avales",
         tracking=1,
@@ -109,3 +147,21 @@ class SaleOrder(models.Model):
             for line in record.order_line:
                 total_base += line.qty_invoiced * line.price_reduce_taxexcl
             record.x_total = total_base
+
+    @api.depends ( 'margin', 'x_costes', 'amount_untaxed', 'margin_percent' )
+    def _compute_x_margen(self) :
+        for record in self :
+            if record.x_costes > 0 :
+                try :
+                    record.x_margen = (record.margin - record.x_costes) / record.amount_untaxed
+                except ZeroDivisionError :
+                    record.x_margen = 0
+            else :
+                record.x_margen = record.margin_percent
+
+    @api.depends ( 'activity_ids' )
+    def _compute_sale_activity_count(self) :
+        for record in self :
+            record.x_sale_order_id_sale_activity_count = self.env['sale.activity'].search_count ( [
+                ('sale_order_id', '=', record.id)
+            ] )
